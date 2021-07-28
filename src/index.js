0b4e40b551d0dd14e74f4131ccebdaea78b4ca42
aldhairescobar <aldhair.escobar@gmail.com>
2021-07-27 21:05:41 -0500

    re-rendering, REACT DOM

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.render(
+      <App counter={counter} />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
+
 
