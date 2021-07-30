8371596aa9f349141f265433092d53a0ab18f6ac
aldhairescobar <aldhair.escobar@gmail.com>
2021-07-29 23:23:29 -0500

    Event handler, passing state to child components

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import React, { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  /* const handleClick = () => {
-    console.log('Clicked')
-  } */
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <>
-      <div>{counter}</div>
-      <button 
-      onClick={() => setCounter(counter + 1)}
-      >
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button 
+        onClick={props.onClick}
+      >
+        {props.text}
+      </button>
   )
 }
 
