4c368df02ed7a02013e63d638fbff9a6c46d8ecb
aldhairescobar <aldhair.escobar@gmail.com>
2021-07-31 23:22:07 -0500

    complex state, using spread operator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  /* const [ left, setLeft ] = useState(0)
+  const [ right, setRight ] = useState(0) */
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const [ clicks, setClicks ] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
+
+  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {clicks.left}
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      {clicks.right}
     </>
   )
 }
 
-const Display = ({ counter }) => <div>{counter}</div>
+/* const Display = ({ counter }) => <div>{counter}</div>
 
 const Button = ({ onClick, text }) => (
     <button 
@@ -25,6 +34,6 @@ const Button = ({ onClick, text }) => (
       >
         {text}
     </button>
-)
+) */
 
 export default App
