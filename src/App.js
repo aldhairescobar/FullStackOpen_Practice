a090b10ae309c03f35f61939e31150c20965faaa
aldhairescobar <aldhair.escobar@gmail.com>
2021-08-02 23:46:05 -0500

    function returning function example

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import React, { useState } from 'react'
 const App = () => {
   const [ value, setValue ] = useState(10)
 
-  const hello = (who) => {
-    const handler = () => console.log('hello', who )
-    return handler
-  }
+  const setToValue = (newValue) => () => {
+    setValue(newValue)
+  } 
 
   return (
     <>
       {value}
-      <button onClick={hello('world')}>button</button>
-      <button onClick={hello('react')}>button</button>
-      <button onClick={hello('function')}>button</button>
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </>
   )
 }
