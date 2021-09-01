ca7e1c5aa9fbe72b6f2544c2b0ae1255653cd694
aldhairescobar <aldhair.escobar@gmail.com>
2021-08-31 22:16:39 -0500

    courseinfo exercise 2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,72 @@
-import ReactDOM from 'react-dom'
-import App from './App.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const notes = [
-  {
+const App = () => {
+  const course = {
     id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      }, 
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
-]
 
-ReactDOM.render(
-  <App notes={notes} />,
-  document.getElementById('root')
-)
+  return <Course course={course} />
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content course={course}/>
+    </>
+  )
+}
+
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </div>
+  )
+}
+
+/* const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+} */
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>    
+  )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
